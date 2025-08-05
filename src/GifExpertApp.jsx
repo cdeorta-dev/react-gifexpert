5b49925ecf8668eb2d00e894d4a2ca4cb8bfb9ee
cdeorta-dev <cdeorta.dev@gmail.com>
2025-08-05 11:41:54 -0300

    cambie a amor predeterminado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,7 @@ import { AddCategory,GifGrid } from './components';
 
   export const GifExpertApp = () => {
     
-    const [categories, setCategories] = useState(['One Punch']);
+    const [categories, setCategories] = useState(['Te amo']);
     // const onAddCategory = () => {
     //     console.log('agrega')
     //     let categoriesadd = categories
